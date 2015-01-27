ee7d4e9dca789d3de5ef38d23e8ccd9d21c37f9c
Jean-Luc Geering <jlgeering.13@gmail.com>
2015-01-27 23:28:39 +0100

    better test

--- test/tramboard_clj/core/zvv_tests.clj
+++ test/tramboard_clj/core/zvv_tests.clj
@@ -11,4 +11,14 @@
              (contains {:meta (contains {:station_id "8588078" :station_name "Zürich, Central"})}))
        (fact "returns a list of departures"
              (zvv/transform-response (:central_2228 fixtures)) =>
-             (contains {:departures (contains (contains {:name "31"}))})))
+             (contains {:departures (contains {:zvv_id "32108510"
+                                               :name "31"
+                                               :type "Bus-NF"
+                                               :colors { :fg "#ffffff" :bg "#a5a2c6" }
+                                               :to "Zürich, Hegibachplatz"
+                                               :departure {
+                                                           :scheduled "2015-01-27T22:38:00.000+01:00"
+                                                           :realtime "2015-01-27T22:38:00.000+01:00"}})})))
+
+
+
