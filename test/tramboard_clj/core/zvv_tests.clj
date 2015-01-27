ceed9c67103354607385102390ccad70642905e5
Jean-Luc Geering <jlgeering.15@gmail.com>
2015-01-27 22:58:21 +0100

    first tests

--- test/tramboard_clj/core/zvv_tests.clj
+++ test/tramboard_clj/core/zvv_tests.clj
@@ -2,6 +2,13 @@
   (:use midje.sweet)
   (:require [tramboard-clj.core.zvv :as zvv]))
 
-(fact "dummy test"
-	(+ 1 1) => 2
-	(* 2 2) => 4)
+(def fixtures
+  {:central_2228 (slurp "fixtures/zvv_responses/central_2228.txt")})
+
+(facts "transform zvv response"
+       (fact "extracts station information under meta key"
+             (zvv/transform-response (:central_2228 fixtures)) =>
+             (contains {:meta (contains {:station_id "8588078" :station_name "Zürich, Central"})}))
+       (fact "returns a list of departures"
+             (zvv/transform-response (:central_2228 fixtures)) =>
+             (contains {:departures (contains (contains {:name "31"}))})))
