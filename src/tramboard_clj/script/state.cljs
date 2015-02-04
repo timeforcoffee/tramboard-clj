cf4fbb23635d26e6097dd2ce75f7c24eb3e321e0
François Terrier <francois.terrier@liip.ch>
2015-02-04 15:43:21 +0100

    added readme and documentation

---
+++ src/tramboard_clj/script/state.cljs
@@ -0,0 +1,73 @@
+(ns tramboard-clj.script.state
+  "This namespace deals with state functions and manipulations. The complete state of the app is the
+  state of both split panes. Each pane's state is represented by a state structure contained in
+  a map with 2 named items, like this :
+    :split-states {:state-1 {:state-id :state-1 :state :home :params {} :visible true}
+                   :state-2 {:state-id :state-2 :state :home :params {} :visible false}}
+    :order [:state-1 :state-2]}
+  When referring to complete state, one means the whole structure above. When referring to state, one
+  means one of the 2 states inside the complete state.")
+
+(defn get-state [complete-state state-id]
+  "Returns the state for the given state id"
+  (get (:split-states complete-state) state-id))
+
+(defn is-home [state]
+  (= :home (:state state)))
+
+(defn is-edit [state]
+  (= :edit (:state state)))
+
+(defn is-split [complete-state]
+  (let [split-states (:split-states complete-state)
+        state-1      (get split-states :state-1)
+        state-2      (get split-states :state-2)]
+  (and (:visible state-1) (:visible state-2))))
+
+(defn go-home [state]
+  (assoc state :state :home :params {}))
+
+(defn go-edit [state view-id]
+  (assoc state :state :edit :params {:view-id view-id}))
+
+(defn- go-hide [state]
+  (go-home (assoc state :visible false)))
+
+(defn- go-show [state]
+  (assoc state :visible true))
+
+(defn- get-other-state [complete-state state]
+  "Given a state, returns the other state in complete state."
+  (let [state-1          (get-state complete-state :state-1)
+        state-2          (get-state complete-state :state-2)
+        state-id         (:state-id state)
+        other-state      (if (= state-id :state-1) state-2 state-1)]
+    other-state))
+
+(defn go-toggle-split [complete-state state]
+  "Toggles the split pane. If only 1 pane is currently visible, chanes the state of the other one to visible.
+  If both panes are visible, changes the given state to not visible."
+  (let [state-to-toggle    state
+        other-state        (get-other-state complete-state state)
+        is-split           (is-split complete-state)
+        new-complete-state (if is-split
+                             (assoc-in complete-state [:split-states (:state-id state-to-toggle)] (go-hide state-to-toggle))
+                             (assoc (assoc-in complete-state [:split-states (:state-id other-state)] (go-show other-state))
+                               :order [(:state-id state-to-toggle) (:state-id other-state)]))]
+    new-complete-state))
+
+(defn modify-complete-state [complete-state state fun]
+  "Applies fun to state inside complete-state and returns the new complete state."
+  (let [state-id           (:state-id state)
+        new-state          (fun state)
+        replace-state      (fn [complete-state state]
+                             (assoc-in complete-state [:split-states state-id] new-state))
+        new-complete-state (replace-state complete-state new-state)]
+    new-complete-state))
+
+(defn get-all-states [complete-state]
+  "Returns a vector containing the states for the 2 panes."
+  (let [split-states (:split-states complete-state)
+        state-1      (get split-states :state-1)
+        state-2      (get split-states :state-2)]
+    [state-1 state-2]))
---
+++ src/tramboard_clj/script/time.cljs
@@ -0,0 +1,43 @@
+(ns tramboard-clj.script.time
+  "This namespace contains everything that deals with time and dates, to display the
+  time in minutes for example."
+  (:require [cljs-time.core :refer [minute hour in-minutes interval now after? minus weeks within? days to-default-time-zone]]
+            [cljs-time.format :refer [parse unparse formatters formatter show-formatters]]
+            [cljs-time.coerce :refer [to-long from-long]]))
+
+(def ^{:private true} hour-minute-formatter (formatter "HH:mm"))
+(def ^{:private true} date-formatter (formatter "d MMM yyyy"))
+(def ^{:private true} only-day-formatter (formatter "EEEE"))
+
+(defn- is-within [datetime from length]
+  (within? (interval (minus from length) from) datetime))
+
+(defn- parse-from-date-time-uncached [unparsed-date]
+  (parse (formatters :date-time) unparsed-date))
+
+(defn minutes-from [timestamp from]
+  (if (after? from timestamp)
+    (- (in-minutes (interval timestamp from)))
+    (in-minutes (interval from timestamp))))
+
+(defn format-to-hour-minute [timestamp]
+  (unparse hour-minute-formatter (to-default-time-zone timestamp)))
+
+(defn display-time [timestamp]
+  "Unpure function that decides how this should be displayed depending on the current day"
+  (let [now                (now)
+        datetime           (from-long timestamp)
+        date-format        (unparse date-formatter datetime)
+        hour-minute-format (unparse hour-minute-formatter datetime)
+        is-today           (= date-format (unparse date-formatter now))
+        is-within-a-week   (is-within datetime now (weeks 1))
+        is-yesterday       (is-within datetime now (days 1))
+        display-time       (str
+                             (if is-today "today"
+                               (if is-yesterday "yesterday"
+                                 (if is-within-a-week (str "last " (unparse only-day-formatter datetime))
+                                   (str " on " date-format))))
+                             " at " hour-minute-format)]
+    display-time))
+
+(def parse-from-date-time (memoize parse-from-date-time-uncached))
