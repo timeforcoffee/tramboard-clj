a5ec253fa1d71d6bd7c8897f29b4d6a86d19c32b
François Terrier <francois.terrier@liip.ch>
2015-02-12 20:47:09 +0100

    added opengraph tags

--- src/clj/tramboard_clj/core/views.clj
+++ src/clj/tramboard_clj/core/views.clj
@@ -4,29 +4,39 @@
   (:require [tramboard-clj.core.zvv :as zvv]))
 
 (defn index-page []
-  (html5 {:lang "en"}
-    [:head
-     [:meta {:charset "UTF-8"}]
-     [:meta {:http-equiv "Content-Type"    :content "text/html;charset=utf-8"}]
-     [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
-     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-     [:meta {:name "language" :content "en"}]
-     [:meta {:name "description" :content "Real-time public transport schedule at stops for bus, trains, trams and cable car."}]
-     [:title "Time for Coffee!"]
-     (include-css "//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css")
-     (include-css "//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css")
-     (include-css "/css/styles.css")
-     (javascript-tag (str "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                           })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                           ga('create', '" (or (System/getenv "GA_TRACKING") "dummy") "', 'auto');ga('send', 'pageview');"))
-     (com.newrelic.api.agent.NewRelic/getBrowserTimingHeader)]
-    (into
-      [:body
-       [:div {:id "my-app"}]
-       (com.newrelic.api.agent.NewRelic/getBrowserTimingFooter)]
-      (include-javascript))))
+  (let [description "Real-time public transport schedule at stops in Switzerland for bus, train, tram, cable car..."
+        lang        "en"
+        title       "Time for Coffee!"]
+    (html5 {:lang lang}
+           [:head
+            [:meta {:charset "UTF-8"}]
+            [:meta {:http-equiv "Content-Type"    :content "text/html;charset=utf-8"}]
+            [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
+            [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+            [:meta {:name "language" :content lang}]
+            [:meta {:name "og:locale" :content lang}]
+
+            [:meta {:name "description" :content description}]
+            [:meta {:name "og:description" :content description}]
+
+            [:title "Time for Coffee!"]
+            [:meta {:name "og:title" :content title}]
+            [:meta {:name "og:url" :content "http://www.timeforcoffee.ch/"}]
+
+            (include-css "//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css")
+            (include-css "//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css")
+            (include-css "/css/styles.css")
+            (javascript-tag (str "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                                 ga('create', '" (or (System/getenv "GA_TRACKING") "dummy") "', 'auto');ga('send', 'pageview');"))
+            (com.newrelic.api.agent.NewRelic/getBrowserTimingHeader)]
+           (into
+             [:body
+              [:div {:id "my-app"}]
+              (com.newrelic.api.agent.NewRelic/getBrowserTimingFooter)]
+             (include-javascript)))))
 
 (defn station [id]
   {:headers {"Content-Type" "application/json; charset=utf-8"}
