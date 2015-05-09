5b1a81d6f3831923398c2fa671ec0cead36e104b
François Terrier <fterrier@gmail.com>
2015-05-09 23:55:23 +0200

    added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,11 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
 var less		= require('gulp-less');
-
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-});
-
-// or...
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        proxy: "yourlocal.dev"
-    });
-});
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('less', function () {
   return gulp.src('src/less/styles.less')
     .pipe(less())
+    .pipe(autoprefixer())
     .pipe(gulp.dest('resources/public/css'));
 });
 
