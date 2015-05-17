e7869f01b8fca4a9c0654ee076e37662543d9894
François Terrier <fterrier@gmail.com>
2015-05-17 21:23:59 +0200

    fixed autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ var autoprefixer = require('gulp-autoprefixer');
 gulp.task('less', function () {
   return gulp.src('src/less/styles.less')
     .pipe(less())
-    .pipe(autoprefixer("> 1%", "last 2 versions", "Firefox ESR", "Opera 12.1", "BlackBerry 10", "Android 4"))
+    .pipe(autoprefixer({
+    	browsers: ["> 1%", "last 2 versions", "Firefox ESR", "Opera 12.1", "BlackBerry 10", "Android 4"]
+    }))
     .pipe(gulp.dest('resources/public/css'));
 });
 
