c59510a09766913a110bb5a30e26c4709171df84
Christian Stocker <me@chregu.tv>
2015-10-21 10:08:07 +0200

    put switzerland on top

--- src/cljs/tramboard_clj/script/util.cljs
+++ src/cljs/tramboard_clj/script/util.cljs
@@ -2,14 +2,16 @@
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (defonce locations
-  [{:id :ch_zh  :name "Zurich City & Canton" :short-label "Zurich"             :flag-class "ch_zh" :api "zvv" :active true}
+  [
+   {:id :ch     :name "Whole Switzerland"  :short-label "Switzerland" :flag-class "ch"    :api "ch" :active true}
+   {:id :ch_zh  :name "Zurich City & Canton" :short-label "Zurich"             :flag-class "ch_zh" :api "zvv" :active true}
    {:id :ch_ge  :name "Geneva City & Canton" :short-label "Geneva"             :flag-class "ch_ge" :api "gva" :active true}
    {:id :ch_lu  :name "Lucerne & parts of Central Switzerland" :short-label "Lucerne" :flag-class "ch_lu" :api "vbl" :active true}
    {:id :ch_bs  :name "Basel City" :short-label "Basel"   :flag-class "ch_bs" :api "bvb" :active true}
    ;{:id :ch_nw  :name "Nordwestschweiz" :short-label "Nordwestschweiz" :flag-class "ch_bs" :api "blt" :active true}
    ;{:id :ch_os  :name "Ostschweiz" :short-label "Ostschweiz" :flag-class "ch_os" :api "ost" :active true}
-   {:id :ch     :name "Rest of Switzerland"  :short-label "Switzerland" :flag-class "ch"    :api "ch" :active true}])
-
+   ])
+   
 
 
 (defn get-location [location-id]
